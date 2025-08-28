98655821d63699d0d3155b85c1536ed9c480f6fe
neetujangid2004 <neetujangid166@gmail.com>
2025-08-28 19:24:07 +0530

    HangmanGame

---
+++ CodeAlpha_Hangman Game/HangmanGame.py
@@ -0,0 +1,42 @@
+import random
+
+words = ["python", "hangman", "computer", "programming", "coding"]
+
+word = random.choice(words)
+guessed_word = ["_"] * len(word)
+attempts_left = 6
+guessed_letters = []
+
+print("Welcome to Hangman!")
+print("Word to guess: ", "".join(guessed_word))
+
+while attempts_left > 0 and "_" in guessed_word :
+    guess = input("\nGuess a letter: ").lower()
+
+    if len(guess) != 1 or not guess.isalpha():
+        print("Please enter a single valid letter.")
+        continue
+
+    if guess in guessed_letters:
+        print("You already guessed that letter.")
+        continue
+
+    guessed_letters.append(guess)
+
+    if guess in word:
+        print(f"Good job! '{guess}' is in the word.")
+        for i, letter in enumerate(word):
+            if letter == guess:
+                guessed_word[i] = guess
+    else:
+        attempts_left -= 1
+        print(f"Wrong guess! '{guess}' is not in the word.")
+        print(f"Attempts left: {attempts_left}")
+
+    print("Word:", "".join(guessed_word))
+    print("Guessed letters:", ",".join(guessed_letters))
+
+if "_" not in guessed_word:
+    print("\nCongratulations! You guessed the word: ", word)
+else:
+    print("\nGame Over! The word was: ", word)
